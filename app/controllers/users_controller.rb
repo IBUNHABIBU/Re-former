2451137dbb1d209839da544fda5cdd40eb6afb4e
IBUNHABIB <ibunhabib@gmail.com>
2019-12-31 23:03:50 +0300

    removing lint errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+
 class UsersController < ApplicationController
   def new
     @user = User.new
@@ -33,6 +34,7 @@ class UsersController < ApplicationController
   end
 
   private
+  
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
