fd3109cfcdb328cea60a9b911fda4c9917d74364
IBUNHABIB <ibunhabib@gmail.com>
2019-12-31 22:53:02 +0300

    adding flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,12 +2,15 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-  
+  def show
+    @user = User.find(params[:id])
+  end
   def create
     @user = User.new(user_params)
   
     if @user.save
-      redirect_to new_user_path
+      flash[:success] = "Your account is created successfully"
+      redirect_to @user
     else
       render :new
     end
@@ -18,8 +21,8 @@ class UsersController < ApplicationController
   def update
     @user  = User.find(params[:id])
     if @user.update(user_params)
-      flash[:success] = "User successfully updated."
-      redirect_to new_user_path
+      flash[:success] = "Your account is updated successfully"
+      redirect_to @user
     else
       render :edit
     end
