fe27d894746b1e053ea465e549d6021ac411f300
IBUNHABIB <ibunhabib@gmail.com>
2019-12-31 23:01:50 +0300

    remove lint errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,31 @@
+# frozen_string_literal: true
 class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
   def show
     @user = User.find(params[:id])
   end
+
   def create
     @user = User.new(user_params)
-  
     if @user.save
-      flash[:success] = "Your account is created successfully"
+      flash[:success] = 'Your account is created successfully'
       redirect_to @user
     else
       render :new
     end
   end
+
   def edit
-    @user  = User.find(params[:id])
+    @user = User.find(params[:id])
   end
+
   def update
-    @user  = User.find(params[:id])
+    @user = User.find(params[:id])
     if @user.update(user_params)
-      flash[:success] = "Your account is updated successfully"
+      flash[:success] = 'Your account is updated successfully'
       redirect_to @user
     else
       render :edit
@@ -29,7 +33,6 @@ class UsersController < ApplicationController
   end
 
   private
-
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
